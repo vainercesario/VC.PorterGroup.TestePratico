76ad16b222e435a60beea6229d2b83b9baad7392
vainercesario <c.vainer@gmail.com>
2023-08-04 18:28:00 -0300

    [refact]: ajustar visibilidade de configurações e isntalações de modulos auxiliares

---
+++ src/VC.PorterGroup.TestePratico/Modulos/InstalarLog.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+
+namespace VC.PorterGroup.TestePratico;
+
+public static class InstaladorConfig
+{
+    public static IServiceCollection ConfigurarLog(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddSingleton(config);
+
+        Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+        services.AddLogging(logBuilder =>
+        {
+            logBuilder.ClearProviders();
+            logBuilder.AddSerilog(dispose: true);
+        });
+
+        return services;
+    }
+}
