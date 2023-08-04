d9cfb3a4bfbd234d3830c569febed5cbb5515ed0
vainercesario <c.vainer@gmail.com>
2023-08-04 18:28:00 -0300

    [refact]: ajustar visibilidade de configurações e isntalações de modulos auxiliares

---
+++ src/VC.PorterGroup.TestePratico/Modulos/ConfigurarAmbiente.cs
@@ -0,0 +1,35 @@
+﻿namespace VC.PorterGroup.TestePratico.Modulos;
+
+public static class ConfigurarAmbiente
+{
+    public static IApplicationBuilder ConfigurarAmbienteDev(this IApplicationBuilder app)
+    {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "VC.PorterGroup v1"));
+
+        return app;
+    }
+
+    public static IApplicationBuilder ConfigurarAmbienteProducao(this IApplicationBuilder app)
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+
+        return app;
+    }
+
+    public static IApplicationBuilder ConfiguracaoComum(this IApplicationBuilder app)
+    {
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+
+        return app;
+    }
+}
