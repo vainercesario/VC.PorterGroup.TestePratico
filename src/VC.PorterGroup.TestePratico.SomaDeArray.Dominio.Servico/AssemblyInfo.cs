497bec7e6dfcc3d211bd4ae562df6b73db50951c
vainercesario <c.vainer@gmail.com>
2023-08-05 04:12:39 -0300

    [refact]: refatoração de assincronismo dos testes de soma de int de array

---
+++ src/VC.PorterGroup.TestePratico.SomaDeArray.Dominio.Servico/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("5b6b15d0-9981-4682-bb95-e29d96fc116e")]
+[assembly: InternalsVisibleTo("VC.PorterGroup.TestePratico.SomaDeArray.Dominio.Teste")]
