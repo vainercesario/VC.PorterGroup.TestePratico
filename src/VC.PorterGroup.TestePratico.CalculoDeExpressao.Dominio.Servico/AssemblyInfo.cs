ce87a5b4142764dcb642cf1774e52cd103131c04
vainercesario <c.vainer@gmail.com>
2023-08-05 17:27:22 -0300

    [feat]: implementação do serviço para calcular expressão string

---
+++ src/VC.PorterGroup.TestePratico.CalculoDeExpressao.Dominio.Servico/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("73fc5626-000a-40b1-81a0-98b2c7d4de9a")]
+[assembly: InternalsVisibleTo("VC.PorterGroup.TestePratico.CalculoDeExpressao.Dominio.Teste")]
---
+++ src/VC.PorterGroup.TestePratico.CalculoDeExpressao.Dominio.Servico/Modulo/InstalarDependencia.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using VC.PorterGroup.TestePratico.CalculoDeExpressao.Dominio.Interface;
+using VC.PorterGroup.TestePratico.CalculoDeExpressao.Dominio.Servico;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class InstalarDependencias
+{
+    public static IServiceCollection AdicionarCalculoExpressaoServicos(this IServiceCollection services)
+    {
+        services.TryAddScoped<IProcessadorMatematicoServico, ProcessadorMatematicoServico>();
+
+        return services;
+    }
+}
