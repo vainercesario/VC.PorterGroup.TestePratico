78c579708efaad8242cf92785d206e3191a6ea6e
vainercesario <c.vainer@gmail.com>
2023-08-05 20:23:40 -0300

    [feat]: implementação de rota para requisição do cálculo de expressão

---
+++ src/VC.PorterGroup.TestePratico/Controllers/CalculoExpressaoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VC.PorterGroup.TestePratico.CalculoDeExpressao.Dominio.Interface;
+
+namespace VC.PorterGroup.TestePratico.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CalculoExpressaoController : ControllerBase
+{
+    private readonly IProcessadorMatematicoServico _processadorMatematicoServico;
+
+    public CalculoExpressaoController(IProcessadorMatematicoServico processadorMatematicoServico)
+    {
+        this._processadorMatematicoServico = processadorMatematicoServico;
+    }
+
+    [HttpGet]
+    public IActionResult Get(string expressao)
+    {
+        try
+        {
+            return Ok(_processadorMatematicoServico.Calcular(expressao));
+        }
+        catch (Exception ex)
+        {
+            return BadRequest($"Não foi possível realizar a operação: {ex.Message}");
+        }
+    }
+}
