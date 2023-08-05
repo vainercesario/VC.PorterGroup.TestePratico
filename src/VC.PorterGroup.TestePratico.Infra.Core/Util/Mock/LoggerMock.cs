bee1b8ad17c7250fb0791bcd1cad8c4cecea65b0
vainercesario <c.vainer@gmail.com>
2023-08-05 10:58:48 -0300

    [refact]: adicionar ilogger fake para os testes quebrados.

---
+++ src/VC.PorterGroup.TestePratico.Infra.Core/Util/Mock/LoggerMock.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace VC.PorterGroup.TestePratico.Infra.Core.Util.Mock;
+
+public class LoggerMock<T> : ILogger<T>
+{
+    public LoggerMock()
+    {
+        Logs = new List<string>();
+    }
+
+    public List<string> Logs { get; }
+
+    public IDisposable BeginScope<TState>(TState state) => null;
+
+    public bool IsEnabled(LogLevel logLevel) => true;
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+    {
+        var logMessage = formatter(state, exception);
+        Logs.Add(logMessage);
+    }
+}
