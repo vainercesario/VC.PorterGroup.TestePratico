2c80259b48758da948eef57c2442905473d56680
vainercesario <c.vainer@gmail.com>
2023-08-05 03:54:13 -0300

    [refact]: encapsular adição de serviços por módulos.

---
+++ src/VC.PorterGroup.TestePratico.SomaDeArray.Dominio.Servico/Modulo/InstalarDependencia.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using VC.PorterGroup.TestePratico.SomaDeArray.Dominio.Servico;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static partial class InstalarDependencias
+{
+    public static IServiceCollection AdicionarSomaDeArrayServicos(this IServiceCollection services)
+    {
+        services.TryAddScoped<ISomaArrayServico, SomaArrayServico>();
+
+        return services;
+    }
+}
