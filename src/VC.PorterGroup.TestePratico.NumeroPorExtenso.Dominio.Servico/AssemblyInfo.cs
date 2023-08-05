ece6ff5265626cdad795ece07eb9ea6b9c488fb0
vainercesario <c.vainer@gmail.com>
2023-08-05 04:49:54 -0300

    [refact]: transformar serviço NumeroPorExtenso em internal

---
+++ src/VC.PorterGroup.TestePratico.NumeroPorExtenso.Dominio.Servico/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("585e333f-a26e-4bb3-9710-1691b711fa75")]
+[assembly: InternalsVisibleTo("VC.PorterGroup.TestePratico.NumeroPorExtenso.Dominio.Teste")]
