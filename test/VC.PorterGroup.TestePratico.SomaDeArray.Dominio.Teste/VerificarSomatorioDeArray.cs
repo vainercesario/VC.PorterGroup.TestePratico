aa7c898423bba271026aabc1f6177a88070aa7ba
vainercesario <c.vainer@gmail.com>
2023-08-05 03:15:16 -0300

    [test]: criação de teste para o funcional de soma de array

---
+++ test/VC.PorterGroup.TestePratico.SomaDeArray.Dominio.Teste/VerificarSomatorioDeArray.cs
@@ -0,0 +1,15 @@
+﻿namespace VC.PorterGroup.TestePratico.SomaDeArray.Dominio.Teste;
+
+[TestClass]
+public class VerificarSomatorioDeArray
+{
+    [TestMethod]
+    public void ValidarSoma()
+    {
+        var lista = new List<int>() { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+        ISomaArrayService somaArray = new SomaArrayService();
+
+        Assert.AreEqual(lista.Sum(), somaArray.Somar(lista));
+    }
+}
