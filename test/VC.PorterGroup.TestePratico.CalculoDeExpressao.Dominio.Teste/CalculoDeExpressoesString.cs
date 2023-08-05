743d9b8b14acd68b1a6d0832b7f6223007b62c0d
vainercesario <c.vainer@gmail.com>
2023-08-05 20:12:25 -0300

    [refact]: correções no projeto apontadas pelos testes.

--- test/VC.PorterGroup.TestePratico.CalculoDeExpressao.Dominio.Teste/CalculoDeExpressoesString.cs
+++ test/VC.PorterGroup.TestePratico.CalculoDeExpressao.Dominio.Teste/CalculoDeExpressoesString.cs
@@ -12,21 +12,22 @@ public class CalculoDeExpressoesString
     {
         var fakeLogger = new LoggerMock<ProcessadorMatematicoServico>();
 
-        List<(string expressao, decimal resultado)> listaDeTestes = new()
+        List<(string expressao, double resultado)> listaDeTestes = new()
         {
-            ("1 + 1", 2m),
-            ("2 * 3 + 4", 10m),
-            ("2 + 3 * 4", 14m),
-            ("3 / 2 * 2", 3m),
-            ("3 / 2", 1.5m),
-            ("2 + 2 + 4 + 10 * 2 / 4", 13m),
-            ("2 + 2 / 4 + 10 * 2 / 4", 7.25m),
+            ("- 1 + 1", 0),
+            ("1 + 1", 2),
+            ("2 * 3 + 4", 10),
+            ("2 + 3 * 4", 14),
+            ("3 / 2 * 2", 3),
+            ("3 / 2", 1.5),
+            ("2 + 2 + 4 + 10 * 2 / 4", 13),
+            ("2 + 2 / 4 + 10 * 2 / 4", 7.5),
         };
 
-        ProcessadorMatematicoServico processadorMatematicoService = new ProcessadorMatematicoServico(fakeLogger);
+        ProcessadorMatematicoServico processadorMatematicoService = new(fakeLogger);
 
-        foreach (var obj in listaDeTestes)
-            Assert.AreEqual(obj.resultado, processadorMatematicoService.Calcular(obj.expressao));
+        foreach (var (expressao, resultado) in listaDeTestes)
+            Assert.AreEqual(resultado, processadorMatematicoService.Calcular(expressao));
     }
 
     [TestMethod]
@@ -36,17 +37,17 @@ public class CalculoDeExpressoesString
 
         List<(string expressao, string erro)> listaDeTestes = new()
         {
-            ("2 * + 4", "A expressão é inválida"),
-            ("* 0 + 2 * 3", "A expressão é inválida"),
-            ("2 * 3 abcd", "A expressão é inválida")
+            ("2 * + 4", "A expressão é inválida."),
+            ("* 0 + 2 * 3", "A expressão é inválida."),
+            ("2 * 3 abcd", "A expressão é inválida.")
         };
 
-        ProcessadorMatematicoServico processadorMatematicoService = new ProcessadorMatematicoServico(fakeLogger);
+        ProcessadorMatematicoServico processadorMatematicoService = new(fakeLogger);
 
-        foreach (var obj in listaDeTestes)
+        foreach (var (expressao, erro) in listaDeTestes)
         {
-            Exception excecao = Assert.ThrowsException<ArithmeticException>(() => processadorMatematicoService.Calcular(obj.expressao));
-            Assert.AreEqual(obj.erro, excecao.Message);
+            Exception excecao = Assert.ThrowsException<ArithmeticException>(() => processadorMatematicoService.Calcular(expressao));
+            Assert.AreEqual(erro, excecao.Message);
         }
     }
 
@@ -56,16 +57,16 @@ public class CalculoDeExpressoesString
 
         List<(string expressao, string erro)> listaDeTestes = new()
         {
-            ("1 / 0", "A expressão possui divisão por zero, não é possível calcular."),
-            ("1 + 1 + 1 / 0", "A expressão possui divisão por zero, não é possível calcular."),
+            ("1 / 0", "Não é possível realizar divisão por zero."),
+            ("1 + 1 + 1 / 0", "Não é possível realizar divisão por zero."),
         };
 
         IProcessadorMatematicoServico processadorMatematicoService = new ProcessadorMatematicoServico(fakeLogger);
 
-        foreach (var obj in listaDeTestes)
+        foreach (var (expressao, erro) in listaDeTestes)
         {
-            Exception excecao = Assert.ThrowsException<ArithmeticException>(() => processadorMatematicoService.Calcular(obj.expressao));
-            Assert.AreEqual(obj.erro, excecao.Message);
+            Exception excecao = Assert.ThrowsException<ArithmeticException>(() => processadorMatematicoService.Calcular(expressao));
+            Assert.AreEqual(erro, excecao.Message);
         }
     }
 }
