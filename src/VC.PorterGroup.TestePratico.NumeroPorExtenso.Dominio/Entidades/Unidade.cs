1d769c6a3e33c1dd19f06d57a036e8a2869b3976
vainercesario <c.vainer@gmail.com>
2023-08-04 21:19:23 -0300

    [feat]: composição das unidades, dezenas e centenas por extenso com decorator

---
+++ src/VC.PorterGroup.TestePratico.NumeroPorExtenso.Dominio/Entidades/Unidade.cs
@@ -0,0 +1,19 @@
+﻿using VC.PorterGroup.TestePratico.NumeroPorExtenso.Dominio.Base;
+
+namespace VC.PorterGroup.TestePratico.NumeroPorExtenso.Dominio.Entidades;
+
+public class Unidade : NumeralComponente
+{
+    private static readonly string[] UNIDADES = { "", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove" };
+
+    public Unidade(int numero)
+        : base(numero) { }
+
+    public override string EscreverPorExtenso()
+    {
+        if (Dezena == 1)
+            return "";
+
+        return $"{((Unidade > 0 && (Dezena > 1 || (Centena > 1 && Dezena == 0))) ? " e " : "")}{UNIDADES[Unidade]}";
+    }
+}
---
+++ test/VC.PorterGroup.TestePratico.Teste/NumerosPorExtenso/VerificarUnidadesDeMilharPorExtenso.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VC.PorterGroup.TestePratico.Teste.NumerosPorExtenso;
+internal class VerificarUnidadesDeMilharPorExtenso
+{
+}
