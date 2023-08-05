a8944361d2f773ca80d89f0371c4c8d7196a8dcb
vainercesario <c.vainer@gmail.com>
2023-08-05 04:38:47 -0300

    [feat]: montar api para receber requisição de soma de array de inteiros.

---
+++ src/VC.PorterGroup.TestePratico/Controllers/SomaDeArrayController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VC.PorterGroup.TestePratico.SomaDeArray.Dominio.Servico;
+
+namespace VC.PorterGroup.TestePratico.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SomaDeArrayController : ControllerBase
+{
+    private readonly ISomaArrayServico _somaArrayServico;
+
+    public SomaDeArrayController(ISomaArrayServico somaArrayServico)
+    {
+        this._somaArrayServico = somaArrayServico;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] InteirosViewModel lista, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return Ok(await _somaArrayServico.SomarAsync(lista.Numeros, cancellationToken));
+        }
+        catch
+        {
+            return BadRequest("Ocorreu erro ao processar a informação.");
+        }
+    }
+}
+
+public class InteirosViewModel
+{
+    public List<int> Numeros { get; set; }
+}
