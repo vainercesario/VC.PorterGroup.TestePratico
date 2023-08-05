1f643e9f646acd0cb539a35546a6477f2fbeb003
vainercesario <c.vainer@gmail.com>
2023-08-04 21:19:23 -0300

    [feat]: composição das unidades, dezenas e centenas por extenso com decorator

---
+++ src/VC.PorterGroup.TestePratico.NumeroPorExtenso.Dominio/Base/NumeralComponente.cs
@@ -0,0 +1,20 @@
+﻿namespace VC.PorterGroup.TestePratico.NumeroPorExtenso.Dominio.Base;
+
+public abstract class NumeralComponente
+{
+    public NumeralComponente(int numero)
+    {
+        Numero = numero;
+
+        Unidade = Numero.RetornaByteDaPosicao(CasasDeValores.Unidade);
+        Dezena = (byte)((Numero.TotalItens() >= 2) ? Numero.RetornaByteDaPosicao(CasasDeValores.Dezena) : 0);
+        Centena = (byte)((Numero.TotalItens() == 3) ? Numero.RetornaByteDaPosicao(CasasDeValores.Centena) : 0);
+    }
+
+    public int Numero { get; private set; }
+    public byte Unidade { get; private set; }
+    public byte Dezena { get; private set; }
+    public byte Centena { get; private set; }
+
+    public abstract string EscreverPorExtenso();
+}
